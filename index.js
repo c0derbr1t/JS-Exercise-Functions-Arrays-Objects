64f8a5c18444bbf66d0f26f3f8663e357dda32ae
c0der.br1t <c0der.br1t@gmail.com>
2019-11-11 14:31:25 -0600

    3 tests passing, including example. Finished 'getName()'

--- index.js
+++ index.js
@@ -61,8 +61,11 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(makePersonObject) {
+// console.log(makePersonObject.name);
+const personName = makePersonObject.name;
+
+  return "Hello, my name is " + personName;
 }
 
 /**
