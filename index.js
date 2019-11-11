d206163f7008be0c076e5ce3be0581b5d7fb2312
c0der.br1t <c0der.br1t@gmail.com>
2019-11-11 16:46:15 -0600

    13 tests passing, including example. Finished 'sortCarInventory()'

--- index.js
+++ index.js
@@ -214,10 +214,18 @@ function getCarInfoById(arr, num) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
+function sortCarInventory(arr) {
+  let carsSorted = arr.sort(function(a,b) {
+    if (a.car_model < b.car_model) {
+      return -1;
+    }
+    if (a.car_model > b.car_model) {
+      return 1;
+    }
+    return 0;
+  })
+  return carsSorted;
 }
-
 /**
  * ### Challenge `getModelYears`
  * 
