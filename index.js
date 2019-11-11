166cf81d194925854d1430765642bc29f28b12fe
c0der.br1t <c0der.br1t@gmail.com>
2019-11-11 17:27:51 -0600

    21 tests passing, including example. Finished all 3 refactoring into arrow functions

--- index.js
+++ index.js
@@ -314,9 +314,13 @@ function getGermanCars(arr) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a, b) => {
+  return a + b;
+}; // code here!
+const addFive = (num) => num + 5; // code here!
+const argTimesTwo = (num) => {
+  return num * 2;
+}; // code here!
 
 /**
  * ### Challenge `carMaker`
